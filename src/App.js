1ce630bd77019840196c86ff90b6f07529020b53
Karl Dudley <karldudley@gmail.com>
2023-06-16 13:47:53 +0100

    update media query

--- src/App.js
+++ src/App.js
@@ -1,47 +1,46 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-import RepoCard from "./components/RepoCard"
+import RepoCard from "./components/RepoCard";
 
-import { Typography } from '@mui/material';
-import Button from '@mui/material/Button';
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
-import TextField from '@mui/material/TextField';
-import Alert from '@mui/material/Alert';
-import Masonry from 'react-masonry-css'
+import { Typography } from "@mui/material";
+import Button from "@mui/material/Button";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import TextField from "@mui/material/TextField";
+import Alert from "@mui/material/Alert";
+import Masonry from "react-masonry-css";
 
-import './App.css';
-import github from './images/github.png';
+import "./App.css";
+import github from "./images/github.png";
 
 function App() {
-  const [name, setName] = useState('')
-  const [owner, setOwner] = useState('')
-  const [ownerUrl, setOwnerUrl] = useState('')
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState(false)
-  const [repos, setRepos] = useState([])
+  const [name, setName] = useState("");
+  const [owner, setOwner] = useState("");
+  const [ownerUrl, setOwnerUrl] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [repos, setRepos] = useState([]);
 
   const breakpoints = {
     default: 4,
     1500: 3,
     1100: 2,
-    700: 1
-  }
+    600: 1,
+  };
 
   useEffect(() => {
     setRepos([]);
-    setOwner('')
-    setOwnerUrl('')
-  }, [name])
-
+    setOwner("");
+    setOwnerUrl("");
+  }, [name]);
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     if (name) {
       searchRepos();
     }
-  }
+  };
 
   const searchRepos = () => {
     setLoading(true);
@@ -49,81 +48,72 @@ function App() {
     axios({
       method: "get",
       url: `https://api.github.com/users/${name}/repos`,
-    }).then (res => {
-        setError(false)
+    })
+      .then((res) => {
+        setError(false);
         setLoading(false);
         setRepos(res.data);
-        setOwner(res.data[0].owner.avatar_url)
-        setOwnerUrl(res.data[0].owner.html_url)
-        
-    }).catch((error) => {
+        setOwner(res.data[0].owner.avatar_url);
+        setOwnerUrl(res.data[0].owner.html_url);
+      })
+      .catch((error) => {
         setLoading(false);
-        setError(true)
-    });
-  }
+        setError(true);
+      });
+  };
 
   const renderRepo = (repo) => {
     return (
       <div key={repo.id}>
-        <RepoCard repo={repo}/>
+        <RepoCard repo={repo} />
       </div>
     );
-  }
+  };
 
   return (
     <div className="page">
-
-      <Typography
-        variant="h5"
-        color="textPrimary"
-        gutterBottom
-      >
+      <Typography variant="h5" color="textPrimary" gutterBottom>
         Search GitHub Repos
       </Typography>
-      <form onSubmit={handleSubmit} noValidate autoComplete='off'>
+      <form onSubmit={handleSubmit} noValidate autoComplete="off">
         <TextField
           onChange={(e) => setName(e.target.value)}
           label="GitHub Username"
           variant="outlined"
-          style = {{width: 230}}
+          style={{ width: 230 }}
           sx={{
             ".MuiOutlinedInput-root": {
-              flexDirection: "row"
+              flexDirection: "row",
             },
             img: {
-              paddingRight: "0.5rem"
-            }
+              paddingRight: "0.5rem",
+            },
           }}
           InputProps={{
-            startAdornment: (
-              <img
-                src={github}
-                alt="GitHub Icon"
-                width="30"
-              />
-            )
+            startAdornment: <img src={github} alt="GitHub Icon" width="30" />,
           }}
           placeholder="Enter Username"
           color="primary"
           required
           spellCheck="false"
-      />
-      <div className="pic">
-        <a href={ownerUrl} target="blank">
-          <img src={owner} alt="" width="100"/>
-        </a>
-      </div>
-        <br /><br />
+        />
+        <div className="pic">
+          <a href={ownerUrl} target="blank">
+            <img src={owner} alt="" width="100" />
+          </a>
+        </div>
+        <br />
+        <br />
         <Button
           type="submit"
           color="primary"
           variant="contained"
-          endIcon={<KeyboardArrowRightIcon/>}
+          endIcon={<KeyboardArrowRightIcon />}
         >
-        {loading ? "Searching..." : "Search"}
+          {loading ? "Searching..." : "Search"}
         </Button>
-        <br/><br/>
-        
+        <br />
+        <br />
       </form>
       <br />
       <Masonry
@@ -131,12 +121,13 @@ function App() {
         className="my-masonry-grid"
         columnClassName="my-masonry-grid_column"
       >
-        {(error)  ? <Alert severity="error">This username could not be found.</Alert> 
-                  : repos.map(renderRepo)
-        }
+        {error ? (
+          <Alert severity="error">This username could not be found.</Alert>
+        ) : (
+          repos.map(renderRepo)
+        )}
       </Masonry>
-
-      </div>
+    </div>
   );
 }
 
